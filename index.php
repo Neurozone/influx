a5dbeae8f5e62d5e2502e6ea5c96c44b7f1c973e
Neurozone <5229035+Neurozone@users.noreply.github.com>
2019-07-15 08:02:22 +0200

    suppression de la page article et renommage de action.php en cron.php

--- index.php
+++ index.php
@@ -835,6 +835,92 @@ $router->get('/favorites', function () use (
 
 $router->mount('/article', function () use ($router, $twig, $myUser, $configurationManager, $feedManager, $folderManager, $cookiedir, $eventManager, $logger) {
 
+    /*
+     * Plugin::callHook("index_pre_treatment", array(&$_));
+
+$view = "article";
+$articleConf = array();
+//recuperation de tous les flux
+$allFeeds = $feedManager->getFeedsPerFolder();
+$tpl->assign('allFeeds',$allFeeds);
+$scroll = isset($_['scroll']) ? $_['scroll'] : 0;
+$tpl->assign('scrollpage',$scroll);
+// récupération des variables pour l'affichage
+$articleConf['articlePerPages'] = $configurationManager->get('articlePerPages');
+$articleDisplayLink = $configurationManager->get('articleDisplayLink');
+$articleDisplayDate = $configurationManager->get('articleDisplayDate');
+$articleDisplayAuthor = $configurationManager->get('articleDisplayAuthor');
+$articleDisplayHomeSort = $configurationManager->get('articleDisplayHomeSort');
+$articleDisplayFolderSort = $configurationManager->get('articleDisplayFolderSort');
+$articleDisplayMode = $configurationManager->get('articleDisplayMode');
+$optionFeedIsVerbose = $configurationManager->get('optionFeedIsVerbose');
+
+$tpl->assign('articleDisplayAuthor',$articleDisplayAuthor);
+$tpl->assign('articleDisplayDate',$articleDisplayDate);
+$tpl->assign('articleDisplayLink',$articleDisplayLink);
+$tpl->assign('articleDisplayMode',$articleDisplayMode);
+
+if(isset($_['hightlighted'])) {
+    $hightlighted = $_['hightlighted'];
+    $tpl->assign('hightlighted',$hightlighted);
+}
+
+$tpl->assign('time',$_SERVER['REQUEST_TIME']);
+
+$target = '`'.MYSQL_PREFIX.'event`.`title`,`'.MYSQL_PREFIX.'event`.`unread`,`'.MYSQL_PREFIX.'event`.`favorite`,`'.MYSQL_PREFIX.'event`.`feed`,';
+if($articleDisplayMode=='summary') $target .= '`'.MYSQL_PREFIX.'event`.`description`,';
+if($articleDisplayMode=='content') $target .= '`'.MYSQL_PREFIX.'event`.`content`,';
+if($articleDisplayLink) $target .= '`'.MYSQL_PREFIX.'event`.`link`,';
+if($articleDisplayDate) $target .= '`'.MYSQL_PREFIX.'event`.`pubdate`,';
+if($articleDisplayAuthor) $target .= '`'.MYSQL_PREFIX.'event`.`creator`,';
+$target .= '`'.MYSQL_PREFIX.'event`.`id`';
+
+$nblus = isset($_['nblus']) ? $_['nblus'] : 0;
+$articleConf['startArticle'] = ($scroll*$articleConf['articlePerPages'])-$nblus;
+if ($articleConf['startArticle'] < 0) $articleConf['startArticle']=0;
+$action = $_['action'];
+$tpl->assign('action',$action);
+
+$filter = array();
+Plugin::callHook("article_pre_action", array(&$_,&$filter,&$articleConf));
+switch($action){
+
+case 'selectedFeed':
+        $currentFeed = $feedManager->getById($_['feed']);
+        $allowedOrder = array('date'=>'pubdate DESC','older'=>'pubdate','unread'=>'unread DESC,pubdate DESC');
+        $order = (isset($_['order'])?$allowedOrder[$_['order']]:$allowedOrder['unread']);
+        $events = $currentFeed->getEvents($articleConf['startArticle'],$articleConf['articlePerPages'],$order,$target,$filter);
+    break;
+
+    case 'selectedFolder':
+        $currentFolder = $folderManager->getById($_['folder']);
+        if($articleDisplayFolderSort) {$order = '`'.MYSQL_PREFIX.'event`.`pubdate` desc';} else {$order = '`'.MYSQL_PREFIX.'event`.`pubdate` asc';}
+        $events = $currentFolder->getEvents($articleConf['startArticle'],$articleConf['articlePerPages'],$order,$target,$filter);
+    break;
+
+    case 'favorites':
+        $filter['favorite'] = 1;
+        //$events = $eventManager->loadAllOnlyColumn($target,$filter,'pubdate DESC',$articleConf['startArticle'].','.$articleConf['articlePerPages']);
+    break;
+
+    case 'unreadEvents':
+    default:
+        $filter['unread'] = 1;
+        if($articleDisplayHomeSort) {$order = 'pubdate desc';} else {$order = 'pubdate asc';}
+        if($optionFeedIsVerbose) {
+            $events = $eventManager->loadAllOnlyColumn($target,$filter,$order,$articleConf['startArticle'].','.$articleConf['articlePerPages']);
+        } else {
+            $events = $eventManager->getEventsNotVerboseFeed($articleConf['startArticle'],$articleConf['articlePerPages'],$order,$target);
+        }
+        break;
+}
+$tpl->assign('events',$events);
+$tpl->assign('scroll',$scroll);
+$view = "article";
+Plugin::callHook("index_post_treatment", array(&$events));
+$html = $tpl->draw($view);
+     */
+
     /* ---------------------------------------------------------------- */
     // Route: /article
     /* ---------------------------------------------------------------- */
@@ -854,6 +940,20 @@ $router->mount('/article', function () use ($router, $twig, $myUser, $configurat
     /* ---------------------------------------------------------------- */
 
     $router->get('/favorites', function () use ($twig, $myUser, $configurationManager, $feedManager, $folderManager, $cookiedir) {
+
+        if (!$_SESSION['user']) {
+            header('location: /login');
+        }
+
+        header('location: /settings/manage');
+
+    });
+
+    /* ---------------------------------------------------------------- */
+    // Route: /article/unread
+    /* ---------------------------------------------------------------- */
+
+    $router->get('/unread', function () use ($twig, $myUser, $configurationManager, $feedManager, $folderManager, $cookiedir) {
 
         if (!$_SESSION['user']) {
             header('location: /login');
@@ -925,7 +1025,7 @@ $router->mount('/settings', function () use ($router, $twig, $trans, $logger, $c
     });
 
     /* ---------------------------------------------------------------- */
-    // Route: /settings/plugin/changeState/{name}/state/{state}
+    // Route: /settings/{option}
     /* ---------------------------------------------------------------- */
 
     $router->get('/{option}', function ($option) use ($twig, $trans, $logger, $config, $cookiedir) {
