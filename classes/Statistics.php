48a47df150448903558379c3257d91a53e2dde7d
Neurozone <5229035+Neurozone@users.noreply.github.com>
2019-08-20 02:22:10 +0200

    formatage du composer.json Création de la classes/Statistics.php

--- classes/Statistics.php
+++ classes/Statistics.php
@@ -30,7 +30,72 @@ class Statistics
 
     public function getStatisticsByLastPublished()
     {
-        $requete = 'select lf.name, FROM_UNIXTIME(max(le.pubdate)) last_published from flux lf inner join items le on lf.id = le.feed group by lf.name order by 2';
+        $results = $this->db->query('select lf.name, FROM_UNIXTIME(max(le.pubdate)) last_published from flux lf inner join items le on lf.id = le.feed group by lf.name order by 2');
+
+        while ($rows = $results->fetch_array()) {
+
+            $stats[] = array(
+                'name' => $rows['name'],
+                'last_published' => $rows['last_published'],
+            );
+
+        }
+
+        return $stats;
+    }
+
+    public function getGlobalStatistics()
+    {
+        $results = $this->db->query('SELECT
+                (SELECT count(1) FROM flux) as nbFlux,
+                (SELECT count(1) FROM items WHERE unread = 1) as nbUnread,
+                (SELECT count(1) FROM items WHERE unread = 0) as nbRead,
+                (SELECT count(1) FROM items) as nbTotal,
+                (SELECT count(1) FROM items WHERE favorite = 1) as nbFavorite
+                ');
+
+        while ($rows = $results->fetch_array()) {
+
+            $stats[] = array(
+                'nbFlux' => $rows['nbFlux'],
+                'nbRead' => $rows['nbRead'],
+                'nbUnread' => $rows['nbUnread'],
+                'nbTotal' => $rows['nbTotal'],
+                'nbFavorite' => $rows['nbFavorite'],
+            );
+
+        }
+
+        return $stats;
+    }
+
+    public function getStatisticsByFeed()
+    {
+        $results = $this->db->query('SELECT name, 
+                count(1) as nbTotal,
+                (SELECT count(1) FROM items le2 WHERE le2.unread=1 and le1.flux = le2.flux) as nbUnread,
+                (SELECT count(1) FROM items le2 WHERE le2.unread=0 and le1.flux = le2.flux) as nbRead,
+                (SELECT count(1) FROM items le2 WHERE le2.favorite=1 and le1.flux = le2.flux) as nbFavorite
+                FROM flux lf1
+                INNER JOIN items le1 on le1.flux = lf1.id
+                GROUP BY name
+                ORDER BY name
+                ');
+
+        while ($rows = $results->fetch_array()) {
+
+            $stats[] = array(
+                'name' => $rows['name'],
+                'nbFlux' => $rows['nbFlux'],
+                'nbRead' => $rows['nbRead'],
+                'nbUnread' => $rows['nbUnread'],
+                'nbTotal' => $rows['nbTotal'],
+                'nbFavorite' => $rows['nbFavorite'],
+            );
+
+        }
+
+        return $stats;
     }
 
 }
